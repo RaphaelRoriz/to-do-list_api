dc8ce8e527876d9f11135b47454bd21465f14165
raphael f r roriz <raphael.roriz@estudante.ufla.br>
2021-03-23 15:27:34 -0300

    commit inicial

---
+++ to_do_list/src/test/java/com/raphael/to_do_list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
